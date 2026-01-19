3c916fa9be39eb0603f5adc5acdff3d22b61317b
Munna kumar <krmunna@gmail.com>
2026-01-19 17:47:35 +0530

    First commit / changes

---
+++ learningreact/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <img src="https://c8.alamy.com/comp/RF64KP/contour-childrens-bunny-simple-drawing-animal-RF64KP.jpg" alt="img" />
+      <h1 className='text-2xl bg-green-500 p-3 rounded '>A card for photos</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores, molestias.</p>
+    </div>
+  )
+}
+
+export default Card
